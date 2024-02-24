ba8772ed9c6e7b622803565e1c833eda2196fbb8
Dayron Rafael <dayronrafael.torres@bwt.ee>
2024-02-24 17:01:38 +0200

    feat: add app scaffolding

---
+++ src/app/presentation/pages/game/game.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game',
+  template: `
+    <p>
+      game works!
+    </p>
+  `,
+  styleUrl: './game.component.scss'
+})
+export class GameComponent {
+
+}
