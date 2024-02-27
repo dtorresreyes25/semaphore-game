1083111b04b41d9de1cdadb46976e72431d1a409
Dayron Rafael <dayronrafael.torres@bwt.ee>
2024-02-27 16:59:09 +0200

    fix: redirect any unknown routes to home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,5 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./presentation/presentation.module').then(m => m.PresentationModule),
   },
+  { path: '**', redirectTo: '/' },
 ];
